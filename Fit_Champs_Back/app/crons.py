c04f779137a6a48a60d7304067016c671198ae9a
pedro <pedroromerorodrigues@outlook.com>
2025-06-11 09:47:50 -0300

    correção nos crons

--- Fit_Champs_Back/app/crons.py
+++ Fit_Champs_Back/app/crons.py
@@ -67,10 +67,10 @@ def schedule_cron_jobs():
                 'refresh_arm_rank_and_update_user',
                 '10 0 * * *',
                 $$
-                    REFRESH MATERIALIZED VIEW arm_rank;
+                    REFRESH MATERIALIZED VIEW rank_arm;
                         
                     UPDATE users AS u SET u.rank_arm = gru.rank
-                    FROM (SELECT ar.id, ar.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM arm_rank AS ar) AS aru
+                    FROM (SELECT ar.id, ar.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM rank_arm AS ar) AS aru
                 $$
             );
         """))
@@ -91,10 +91,10 @@ def schedule_cron_jobs():
                 'refresh_leg_rank_and_update_user',
                 '20 0 * * *',
                 $$
-                    REFRESH MATERIALIZED VIEW leg_rank;
+                    REFRESH MATERIALIZED VIEW rank_leg;
                         
                     UPDATE users AS u SET u.rank_leg = lru.rank
-                    FROM (SELECT lr.id, lr.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM leg_rank AS lr) AS lru
+                    FROM (SELECT lr.id, lr.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM rank_leg AS lr) AS lru
                 $$
             );
         """))
@@ -115,10 +115,10 @@ def schedule_cron_jobs():
                 'refresh_shoulder_rank_and_update_user',
                 '30 0 * * *',
                 $$
-                    REFRESH MATERIALIZED VIEW shoulder_rank;
+                    REFRESH MATERIALIZED VIEW rank_shoulder;
                         
                     UPDATE users AS u SET u.rank_shoulder = sru.rank
-                    FROM (SELECT sr.id, sr.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM shoulder_rank AS sr) AS sru
+                    FROM (SELECT sr.id, sr.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM rank_shoulder AS sr) AS sru
                 $$
             );
         """))
@@ -139,10 +139,10 @@ def schedule_cron_jobs():
                 'refresh_back_rank_and_update_user',
                 '40 0 * * *',
                 $$
-                    REFRESH MATERIALIZED VIEW back_rank;
+                    REFRESH MATERIALIZED VIEW rank_back;
                         
                     UPDATE users AS u SET u.rank_back = bru.rank
-                    FROM (SELECT br.id, br.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM back_rank AS br) AS bru
+                    FROM (SELECT br.id, br.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM rank_back AS br) AS bru
                 $$
             );
         """))
@@ -163,10 +163,10 @@ def schedule_cron_jobs():
                 'refresh_chest_rank_and_update_user',
                 '50 0 * * *',
                 $$
-                    REFRESH MATERIALIZED VIEW chest_rank;
+                    REFRESH MATERIALIZED VIEW rank_chest;
                         
                     UPDATE users AS u SET u.rank_chest = cru.rank
-                    FROM (SELECT cr.id, cr.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM chest_rank AS cr) AS cru
+                    FROM (SELECT cr.id, cr.total_volume, RANK() OVER ( ORDER BY total_volume DESC) FROM rank_chest AS cr) AS cru
                 $$
             );
         """))
